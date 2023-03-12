7d31e207629b3cd9ac60753755fb9b70defe8d4a
KromeBBtw <scheiber.malia-it22@it.dhbw-ravensburg.de>
2023-03-12 11:57:00 +0100

    Adding Main.c
    Adding unfinished Main.c to the repo for testing purposes

--- Main.c
+++ Main.c
@@ -1,43 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct developer_group{
+    char name[7];
+    char alias[8];
+} developer_group;
 
 int main(){
 
 char sel = 0;
 
-struct developer_group
-{
-    char name;
-    int alias;
-};
+developer_group dev1;
+developer_group dev2;
 
-struct developer_group developer1;
-struct developer_group developer2;
+strcpy(dev1.name, "Malian");
+strcpy(dev2.name, "Konrad");
 
-strcpy(developer1.name, "Malian");
-developer1.alias = "Student";
+strcpy(dev1.alias, "Student");
+strcpy(dev2.alias, "Student");
 
-strcpy(developer2.name, "Conrad");
-developer2.alias = "Student";
  
-printf("========================\n
-Choose your action:\n
-List Developers [1]\n
-Print Group Logo [2]\n
-Print Group [3]\n
-Exit [4]\n
+printf("========================\
+Choose your action:\
+List Developers [1]\
+Print Group Logo [2]\
+Print Group [3]\
+Exit [4]\n"
 ========================");
 
 
 switch (scanf("%c", &sel);
 {
 	case1:
-		printf("%c\n", developer1.name);
-		printf("%c\n", developer1.alias);
+		printf("%d\n", developer1.name);
+		printf("%d\n", developer1.alias);
 
-		printf("%c\n", developer2.name);
-		printf("%c\n", developer2.alian);
+		printf("%d\n", developer2.name);
+		printf("%d\n", developer2.alian);
 		break;
 	case2:
 		printf("...\n");
