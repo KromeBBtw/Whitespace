cdd5ac3e0e7ace9019b4074e56bb3feafb8c9f2b
KromeBBtw <scheiber.malia-it22@it.dhbw-ravensburg.de>
2023-03-12 12:47:37 +0100

    Minor changes
    Moving main.c to src

--- src/main.c
+++ src/main.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
+typedef struct developer_group{
+    char name[7];
+    char alias[8];
+} developer_group;
+
+int main(){
+
+char sel = 0;
+
+developer_group dev1;
+developer_group dev2;
+
+strcpy(dev1.name, "Malian");
+strcpy(dev2.name, "Konrad");
+
+strcpy(dev1.alias, "Student");
+strcpy(dev2.alias, "Student");
+
+ 
+printf("========================\
+Choose your action:\
+List Developers [1]\
+Print Group Logo [2]\
+Print Group [3]\
+Exit [4]\n"
+========================");
+
+
+switch (scanf("%c", &sel);
 {
-    return 0;
+	case1:
+		printf("%d\n", developer1.name);
+		printf("%d\n", developer1.alias);
+
+		printf("%d\n", developer2.name);
+		printf("%d\n", developer2.alian);
+		break;
+	case2:
+		printf("...\n");
+		break;
+        case3:
+                printf("...\n");
+                break;
+        case4:
+                printf("...\n");
+                break;  
+        default:
+                printf("...\n");
+                break;
+}
+return 0;
 }
