eafabd5e8bba4d684c09b39916c9f5a64071c6e8
Krome <122440060+KromeBBtw@users.noreply.github.com>
2023-03-20 11:27:22 +0000

    Added comments
    Added comments to all files in /include and the CMakeLists.txt file

--- include/developer.h
+++ include/developer.h
@@ -1,14 +1,18 @@
+// include guards
 #ifndef DEVELOPER_H_
 #define DEVELOPER_H_
 
+// define variables for static numbers to easily change and access them
 #define max_name_length 7
 #define max_alias_length 8
 
+// create a struct to store the developers names and aliases
 typedef struct developer{
     char name[max_name_length];
     char alias[max_alias_length];
 } developer;
 
+// create a function to define the developers names and aliases
 void developer_init(developer *developer, char name[], char alias[]);
 
 #endif
--- include/developer_group.h
+++ include/developer_group.h
@@ -1,23 +1,30 @@
+// include guards
 #ifndef DEVELOPER_GROUP_H_
 #define DEVELOPER_GROUP_H_
 
+// include the developer.h library to use the structs and functions
 #include "developer.h"
 
+// define variables for static numbers to easily change and access them
 #define MAX_LOGO_LENGTH 200
 #define DEVELOPER_AMOUNT 2
 
-
+// declare a variable which can be globally accessed for developer_group
 extern char logo_template[];
 
+// create a struct to store the group logo and developer 
 typedef struct developer_group{
     char logo[MAX_LOGO_LENGTH];
     developer developer[DEVELOPER_AMOUNT];
 } developer_group;
 
+// function to define the developers and their aliases
 void developer_group_init(developer_group *developer_group, char dev1_name[], char dev1_alias[], char dev2_name[], char dev2_alias[]);
 
+// function to print the developers, which have been initialized by the developer_group_init function
 void print_developer(developer developer[]);
 
+// function to print the group logo
 void print_logo(developer_group *developer_group);
 
 
