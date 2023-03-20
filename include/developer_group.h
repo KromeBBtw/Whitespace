9d4f250eb4c5a18a86ae3f70be6f12ecfeb36d0b
Conrad <122440196+cndrw@users.noreply.github.com>
2023-03-20 10:00:54 +0000

    changed developer_group_init
    changed the function to now better fit the description of the exam task

--- include/developer_group.h
+++ include/developer_group.h
@@ -7,6 +7,8 @@
 #define DEVELOPER_AMOUNT 2
 
 
+char logo[MAX_LOGO_LENGTH] = " __  __     __    \n/\\ \\_\\ \\   /\\ \\   \n\\ \\  __ \\  \\ \\ \\ \n \\ \\_\\ \\_\\  \\ \\_\\   \n  \\/_/\\/_/   \\/_/ \n";
+
 typedef struct developer_group{
     char logo[MAX_LOGO_LENGTH];
     developer developer[DEVELOPER_AMOUNT];
